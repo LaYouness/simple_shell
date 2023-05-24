a21fe5f06b3273ffe7e051c89ae42de944ded047
Sahar <Aguidi53@gmail.com>
2023-05-24 07:38:44 -0700

    test

--- 0-simple.c
+++ 0-simple.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("Hello, world!\n");
-    }
+	for (i = 0; i < 10; i++)
+	{
+		printf("Hello, world!\n");
+	}
 
-    return 0;
+		return (0);
 }
